c1bb751e3ff4d6e8b9d65553371d29cd59fd649d
Lyubov Martemyanova <martemyanova.liubov@gmail.com>
2025-02-05 22:04:23 +0300

    delete comments & logs

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,22 +1,14 @@
 import React from 'react';
 
-// Изображения самих проектов отображаются компонентом без состояния ProjectList, которому от Portfolio 
-// мы передаём список проектов — в свойство projects. 
-// Отображение проектов — это единственная ответственность компонента ProjectList.
-
-// в компоненте Portfolio отфильтровать по активному фильтру проекты и передать их в компонент ProjectList
-
 function ProjectList({ projects }) {
-
-  console.log(projects)
 
   return (
     projects.map((project, index) => (
-    <div className="project" key={index}>
-      <img src={project.img} alt={project.category}/>
-    </div>
-  ))
-  )
-}
+      <div className="project" key={index}>
+        <img src={project.img} alt={project.category}/>
+      </div>
+    ))
+  );
+};
 
-export default ProjectList
+export default ProjectList;
--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,28 +1,7 @@
 import React from 'react';
 
-// Сами фильтры отображаются в компоненте без состояния Toolbar, которому от Portfolio мы передаём три свойства:
-// filters — список фильтров, название которых совпадает с категориями проектов;
-// selected — активный фильтр;
-// onSelectFilter(filter) — обработчик события, который реагирует на выбор пользователем одного из фильтров, 
-// обработчик принимает один аргумент — строку с названием фильтра.
-
-// Пример:
-
-// <Toolbar
-//   filters={["All", "Websites", "Flayers", "Business Cards"]}
-//   selected="All"
-//   onSelectFilter={(filter) => {console.log(filter);}}/>
-// В этом примере при выборе фильтра его название будет выведено в консоль. Например, «Business Cards».
-
-// установить состояние выбранного фильтра в обработчике события, который Portfolio передаёт в свойство onSelectFilter компонента Toolbar
-
 function Toolbar (props) {
-    const { filters = [], selected, onSelectFilter } = props
-    // console.log(selected)
-    // console.log(filters)
-
-
-    // console.log(filters)
+    const { filters = [], selected, onSelectFilter } = props;
 
     return (
         <div>
@@ -36,7 +15,7 @@ function Toolbar (props) {
                 </button>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default Toolbar
+export default Toolbar;
